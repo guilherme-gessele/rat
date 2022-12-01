c27809d6b70a42f4861dab08cac188d618c08c04
Gui Gessele <gagessele@outlook.com.br>
2022-12-01 13:43:11 -0300

    added clap

--- src/lib.rs
+++ src/lib.rs
@@ -2,48 +2,38 @@ use std::error::Error;
 use std::fs;
 use std::path::PathBuf;
 
-pub struct Config {
-    program: String,
-    paths: Vec<Box<PathBuf>>,
-}
+pub type Paths = Vec<Box<PathBuf>>;
 
-impl Config {
-    pub fn build(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 2 {
-            return Err("not enough arguments");
-        }
-
-        let program = args[0].clone();
-
-        let paths = args[1..]
-            .into_iter()
-            .map(|p| {
-                let path = PathBuf::from(p);
-                Box::new(path)
-            })
-            .collect();
-
-        Ok(Config { program, paths })
+pub fn parse(files: Vec<String>) -> Result<Paths, &'static str> {
+    if files.len() < 1 {
+        return Err("not enough arguments");
     }
+
+    let paths = files
+        .into_iter()
+        .map(|p| {
+            let path = PathBuf::from(p);
+            Box::new(path)
+        })
+        .collect();
+
+    Ok(paths)
 }
 
-pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
-    for path in &config.paths {
+pub fn run(paths: Paths) -> Result<(), Box<dyn Error>> {
+    for path in paths {
         let path = path.as_path();
 
+        let display = path.display();
+
         if path.is_dir() {
-            println!("{0}: {1}: Is a directory", config.program, path.display());
+            println!("rat: {0}: Is a directory", display);
             continue;
         }
 
         let content = match fs::read_to_string(path) {
             Ok(content) => content,
-            Err(err) => format!(
-                "{0}: Unable to read file {1}: {2}",
-                config.program,
-                path.display(),
-                err
-            ),
+            Err(err) => format!("rat: Unable to read file {0}: {1}", display, err),
         };
 
         println!("{content}");
@@ -52,6 +42,6 @@ pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn err(config: &Config, e: Box<dyn Error>) {
-    println!("{0}: application error: {1}", config.program, e)
+pub fn err(err: Box<dyn Error>) {
+    println!("rat: application error: {0}", err)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
-use rat::Config;
-use std::env;
+use clap::{Args, Parser};
+use rat::Paths;
 use std::process;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(flatten)]
+    command: Command,
+
+    files: Vec<String>,
+}
+
+#[derive(Args, Debug)]
+struct Command {
+    /// display $ at end of each line
+    #[arg(short = 'E', long, default_value_t = false)]
+    show_ends: bool,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let cli = Cli::parse();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
+    let paths: Paths = match rat::parse(cli.files) {
+        Ok(files) => files,
+        Err(err) => {
+            println!("Problem parsing arguments: {err}");
+            process::exit(1);
+        }
+    };
 
-    if let Err(e) = rat::run(&config) {
-        rat::err(&config, e)
+    if let Err(err) = rat::run(paths) {
+        rat::err(err)
     }
 }
